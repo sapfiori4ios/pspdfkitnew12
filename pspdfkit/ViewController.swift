ba032f08bb5e5586d1267c142c3030b3a322c2df
kallesh K C <saplearncf@gmail.com>
2023-03-13 19:38:30 +0530

    PSPDFKit changes

--- pspdfkit/ViewController.swift
+++ pspdfkit/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import PSPDFKit
+import PSPDFKitUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // Update to use your document name.
+        let fileURL = Bundle.main.url(forResource: "Document", withExtension: "pdf")!
+        let document = Document(url: fileURL)
+
+        // The configuration closure is optional and allows additional customization.
+        let pdfController = PDFViewController(document: document) {
+                $0.isPageLabelEnabled = false
+        }
+
+        // Present the PDF view controller within a `UINavigationController` to show built-in toolbar buttons.
+        present(UINavigationController(rootViewController: pdfController), animated: true)
     }
 }
 
